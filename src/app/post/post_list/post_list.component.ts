4cf2c78f456adf89e5da025c89cf556aa3f9b452
anthonyjuarezsolis <anthonyjuarezsolis@gmail.com>
2018-08-09 18:17:16 -0500

    GroupFront
    0.1

---
+++ src/app/post/post_list/post_list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import { Router } from '../../../../node_modules/@angular/router';
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post_list.component.html'
+})
+export class PostListComponent implements OnInit {
+    ngOnInit(): void {
+        this.getPosts();
+    }
+
+    // tslint:disable-next-line:member-ordering
+    public posts: Array<any> = [];
+
+    constructor(private postService: PostService,
+                private router: Router) {
+
+    }
+
+    public getPosts() {
+        this.postService.getPosts().
+            subscribe(data => this.posts = data);
+    }
+
+    public deletePost(id: number) {
+        // console.log('id post', id);
+         this.postService.removePost(id).
+             subscribe(data => this.getPosts()
+                );
+    }
+
+    public onSubmit(id: number) {
+        this.postService.byIdPost(id).
+            subscribe(data => this.router.navigate(['/posts/detail/id']));
+    }
+
+}
